0201f9d3c067f2916b704e83556f56c234f64cb4
BYT <101860864+BYT@users.noreply.github.com>
2026-09-10 17:30:34 +0800

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-var CACHE='fa-v5';
+var CACHE='fa-v6';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}).then(function(){return self.skipWaiting()}))});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}))});
 self.addEventListener('fetch',function(e){
   if(e.request.method!=='GET')return;
-  var u=e.request.url;
-  if(u.indexOf('open-meteo.com')>=0){e.respondWith(fetch(e.request).then(function(r){var c=r.clone();caches.open(CACHE).then(function(ca){ca.put(e.request,c)});return r}).catch(function(){return caches.match(e.request)}));return}
-  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}));
+  e.respondWith(fetch(e.request).then(function(r){var c=r.clone();caches.open(CACHE).then(function(ca){ca.put(e.request,c)});return r}).catch(function(){return caches.match(e.request)}));
 });
