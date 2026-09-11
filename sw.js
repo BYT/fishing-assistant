ed1c31fac5dab2500efedd312206e282bba0406f
BYT <101860864+BYT@users.noreply.github.com>
2026-09-11 16:50:50 +0800

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE='fa-v11';
-var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./tiao-anim.gif','./bait-anim.gif','./fish-anim.gif'];
+var CACHE='fa-v12';
+var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./bait-anim.gif','./fish-anim.gif','./tiao-m1.gif','./tiao-m2.gif','./tiao-m3.gif','./tiao-m4.gif','./tiao-m5.gif','./tiao-m6.gif'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}).then(function(){return self.skipWaiting()}))});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}))});
 self.addEventListener('fetch',function(e){
