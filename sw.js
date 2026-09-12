ea2a61fa7943d6a2095181a9af32d60ee3339345
BYT <101860864+BYT@users.noreply.github.com>
2026-09-12 16:52:14 +0800

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE='fa-v25';
+var CACHE='fa-v26';
 var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./bait-anim.gif','./fish-anim.gif','./tiao-m1-s1.gif','./tiao-m1-s2.gif','./tiao-m1-s3.gif','./tiao-m1-s4.gif','./tiao-m2-s1.gif','./tiao-m2-s2.gif','./tiao-m2-s3.gif','./tiao-m2-s4.gif','./tiao-m3-s1.gif','./tiao-m3-s2.gif','./tiao-m3-s3.gif','./tiao-m3-s4.gif','./tiao-m4-s1.gif','./tiao-m4-s2.gif','./tiao-m4-s3.gif','./tiao-m4-s4.gif','./tiao-m5-s1.gif','./tiao-m5-s2.gif','./tiao-m5-s3.gif','./tiao-m5-s4.gif','./tiao-m6-s1.gif','./tiao-m6-s2.gif','./tiao-m6-s3.gif','./tiao-m6-s4.gif'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}).then(function(){return self.skipWaiting()}))});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}))});
