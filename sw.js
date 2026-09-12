fb11b83e086dade7722597be1342d6c2e0ffd225
BYT <101860864+BYT@users.noreply.github.com>
2026-09-12 09:52:37 +0800

    deploy: sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE='fa-v13';
-var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./bait-anim.gif','./fish-anim.gif','./tiao-m1.gif','./tiao-m2.gif','./tiao-m3.gif','./tiao-m4.gif','./tiao-m5.gif','./tiao-m6.gif'];
+var CACHE='fa-v14';
+var ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./bait-anim.gif','./fish-anim.gif','./tiao-m1-s1.gif','./tiao-m1-s2.gif','./tiao-m1-s3.gif','./tiao-m1-s4.gif','./tiao-m2-s1.gif','./tiao-m2-s2.gif','./tiao-m2-s3.gif','./tiao-m2-s4.gif','./tiao-m3-s1.gif','./tiao-m3-s2.gif','./tiao-m3-s3.gif','./tiao-m3-s4.gif','./tiao-m4-s1.gif','./tiao-m4-s2.gif','./tiao-m4-s3.gif','./tiao-m4-s4.gif','./tiao-m5-s1.gif','./tiao-m5-s2.gif','./tiao-m5-s3.gif','./tiao-m5-s4.gif','./tiao-m6-s1.gif','./tiao-m6-s2.gif','./tiao-m6-s3.gif','./tiao-m6-s4.gif'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS)}).then(function(){return self.skipWaiting()}))});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==CACHE}).map(function(k){return caches.delete(k)}))}).then(function(){return self.clients.claim()}))});
 self.addEventListener('fetch',function(e){
